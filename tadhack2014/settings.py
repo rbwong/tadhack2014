edf314f1f5ae7a11e93dfc11ab3f7a970a1bfe18
vagrant <vagrant@precise32.(none)>
2014-06-06 21:27:41 +0000

    fixed db settings

--- tadhack2014/settings.py
+++ tadhack2014/settings.py
@@ -96,10 +96,10 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    STATIC_PATH,
 )
 TEMPLATE_DIRS = (
     TEMPLATE_PATH,
