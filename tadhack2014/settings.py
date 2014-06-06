ca4592d79bdc21cd31db015a44e97b02c40c309e
vagrant <vagrant@precise32.(none)>
2014-06-06 22:12:36 +0000

    fixed db settings

--- tadhack2014/settings.py
+++ tadhack2014/settings.py
@@ -68,15 +68,9 @@ WSGI_APPLICATION = 'tadhack2014.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 import dj_database_url
-'''
+
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite')
-}'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
