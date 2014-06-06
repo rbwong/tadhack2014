fc88a8edb38502ce828f3dcb85e2a941f7916160
vagrant <vagrant@precise32.(none)>
2014-06-06 21:14:03 +0000

    fixed db settings

--- tadhack2014/settings.py
+++ tadhack2014/settings.py
@@ -68,12 +68,8 @@ WSGI_APPLICATION = 'tadhack2014.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
 }
 
 # Internationalization
@@ -101,7 +97,7 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
